a238398ef01b6918d3c554f42a7b856bc248c997
Barbara Cunha <barbara.torres.cunha@gmail.com>
2026-01-10 17:02:20 -0300

    Funções de adicionar e remover integradas à classe ListaDeTarefas

--- src/ListaDeTarefas.py
+++ src/ListaDeTarefas.py
@@ -17,3 +17,27 @@ class ListaDeTarefas:
     ## Aplicar o ADAPTER por aqui
     def set_lista(self, dados):
         pass
+
+
+    # método para adicionar uma tarefa
+    def adicionar_tarefa(self, tarefa):
+        if not tarefa or tarefa.nome == "":
+            return False
+
+        novo_id = len(self.tarefas_registradas) + 1
+
+        self.tarefas_registradas.append({
+            "id": novo_id,
+            "nome": tarefa.nome,
+            "status": "pendente"
+        })
+        return True
+
+
+    # método para remover uma tarefa pelo ID
+    def remover_tarefa(self, id_tarefa):
+        for tarefa in self.tarefas_registradas:
+            if tarefa["id"] == id_tarefa:
+                self.tarefas_registradas.remove(tarefa)
+                return True
+        return False
