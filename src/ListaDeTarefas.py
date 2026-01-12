c7bd6f93f4c8ea5b81644a24986ccb408effde6b
leticiabborges31-create <leticiabborges31@gmail.com>
2026-01-12 11:07:43 -0300

    Update ListaDeTarefas.py

--- src/ListaDeTarefas.py
+++ src/ListaDeTarefas.py
@@ -20,26 +20,46 @@ class ListaDeTarefas:
 
 
     # método para adicionar uma tarefa
-    def adicionar_tarefa(self, tarefa):
-        if not tarefa or tarefa.nome == "":
+  
+    def adicionar_tarefa(self):
+        nome = input("Digite o nome da tarefa: ").strip()
+        descricao = input("Digite a descrição da tarefa: ").strip()
+
+        if not nome:
+            print(" Nome inválido!")
             return False
 
         novo_id = len(self.tarefas_registradas) + 1
-        tarefa.id = novo_id
 
         self.tarefas_registradas.append({
             "id": novo_id,
-            "nome": tarefa.nome,
-            "status": "pendente",
-            "descricao": tarefa.descricao
+            "nome": nome,
+            "descricao": descricao,
+            "status": "pendente"
         })
+
+        print(f"Tarefa adicionada com sucesso!")
+        print(f"ID {novo_id} - {nome} [pendente]")
         return True
 
+#metodo para remover tarefa
+        def remover_tarefa(self):
+        if not self.tarefas_registradas:
+            print(" Não há tarefas para remover.")
+            return False
 
-    # método para remover uma tarefa pelo ID
-    def remover_tarefa(self, id_tarefa):
-        for tarefa in self.tarefas_registradas:
-            if tarefa["id"] == id_tarefa:
-                self.tarefas_registradas.remove(tarefa)
-                return True
-        return False
+        try:
+            id_tarefa = int(input("Digite o ID da tarefa que deseja remover: "))
+
+            for tarefa in self.tarefas_registradas:
+                if tarefa["id"] == id_tarefa:
+                    self.tarefas_registradas.remove(tarefa)
+                    print(" Tarefa removida com sucesso!")
+                    return True
+
+            print(" ID não encontrado.")
+            return False
+
+        except ValueError:
+            print(" Digite um número válido.")
+            return False
