c6b226b4c9893f2188533f31b82ce68fe2cffc94
leticiabborges31-create <leticiabborges31@gmail.com>
2026-01-12 10:14:28 -0300

    Update add_e_remover_tarefa.py

--- src/add_e_remover_tarefa.py
+++ src/add_e_remover_tarefa.py
@@ -1,17 +1,28 @@
-#método para add tarefa
-    def add_tarefa(self, tarefa):
-        if not tarefa or tarefa.strip() == "":
+class ListaDeTarefas:
+    def __init__(self):
+        self.tarefas_registradas = []
+        self.proximo_id = 1  
+    # método para adicionar tarefa
+    def add_tarefa(self, nome, descricao):
+        if not nome or nome.strip() == "":
             return False
 
         self.tarefas_registradas.append({
-            "nome": tarefa.strip(),
+            "id": self.proximo_id,
+            "nome": nome.strip(),
+            "descricao": descricao.strip(),
             "status": "pendente"
         })
+
+        self.proximo_id += 1
         return True
- 
-    # método para remover a tarefa 
-    def remover_tarefa(self, indice):
-        if 0 <= indice < len(self.tarefas_registradas):
-            self.tarefas_registradas.pop(indice)
-            return True
+
+    # método para remover a tarefa pelo ID
+    def remover_tarefa(self, id_tarefa):
+        for tarefa in self.tarefas_registradas:
+            if tarefa["id"] == id_tarefa:
+                self.tarefas_registradas.remove(tarefa)
+                return True
         return False
+
+
