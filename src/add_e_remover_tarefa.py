1a88c55cad6b905bccaf18cf6ddff339ffeafcdc
leticiabborges31-create <leticiabborges31@gmail.com>
2026-01-12 10:53:00 -0300

    Update add_e_remover_tarefa.py

--- src/add_e_remover_tarefa.py
+++ src/add_e_remover_tarefa.py
@@ -1,28 +1,63 @@
 class ListaDeTarefas:
-    def __init__(self):
-        self.tarefas_registradas = []
-        self.proximo_id = 1  
-    # método para adicionar tarefa
-    def add_tarefa(self, nome, descricao):
-        if not nome or nome.strip() == "":
+    __instance = None
+    tarefas_registradas = []
+
+
+    def __new__(cls):
+        if ListaDeTarefas.__instance is None:
+            ListaDeTarefas.__instance = super().__new__(cls)
+        return ListaDeTarefas.__instance
+
+    # Método para obter a lista
+    def get_lista(self):
+        return self.tarefas_registradas
+
+
+    def set_lista(self, dados):
+        self.tarefas_registradas = dados
+
+    # MÉTODO PARA ADICIONAR TAREFA (COM INPUT)
+    def adicionar_tarefa(self):
+        nome = input("Digite o nome da tarefa: ").strip()
+        descricao = input("Digite a descrição da tarefa: ").strip()
+
+        if not nome:
+            print(" Nome inválido!")
             return False
 
+        novo_id = len(self.tarefas_registradas) + 1
+
         self.tarefas_registradas.append({
-            "id": self.proximo_id,
-            "nome": nome.strip(),
-            "descricao": descricao.strip(),
+            "id": novo_id,
+            "nome": nome,
+            "descricao": descricao,
             "status": "pendente"
         })
 
-        self.proximo_id += 1
+        print(f"Tarefa adicionada com sucesso!")
+        print(f"ID {novo_id} - {nome} [pendente]")
         return True
 
-    # método para remover a tarefa pelo ID
-    def remover_tarefa(self, id_tarefa):
-        for tarefa in self.tarefas_registradas:
-            if tarefa["id"] == id_tarefa:
-                self.tarefas_registradas.remove(tarefa)
-                return True
-        return False
+    # MÉTODO PARA REMOVER TAREFA (COM INPUT)
+    def remover_tarefa(self):
+        if not self.tarefas_registradas:
+            print(" Não há tarefas para remover.")
+            return False
+
+        try:
+            id_tarefa = int(input("Digite o ID da tarefa que deseja remover: "))
+
+            for tarefa in self.tarefas_registradas:
+                if tarefa["id"] == id_tarefa:
+                    self.tarefas_registradas.remove(tarefa)
+                    print(" Tarefa removida com sucesso!")
+                    return True
+
+            print(" ID não encontrado.")
+            return False
+
+        except ValueError:
+            print(" Digite um número válido.")
+            return False
 
 
